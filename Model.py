8db029c5703491201f507479d7e06e88e1cf6aeb
“tangqingcool2004@163.com” <tangqingcool200@163.com>
2025-03-18 22:52:23 +0800

    修改main和model

--- Model.py
+++ Model.py
@@ -1,3 +1,4 @@
+# Model.py
 import torch
 from torch import nn
 from torch.distributions import Normal
@@ -7,25 +8,23 @@ import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
 import math
-
 from torch.autograd import Variable
 
-from Graph import returnA
+# 删除不再需要的import
+# from Graph import returnA  
 from utils import GCN_Loss
 
 
 class GraphConvolution(Module):
-    """
-    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
-    """
+    """（保持原样，但实际已不再使用）"""
 
     def __init__(self, in_features, out_features, bias=True):
-        super(GraphConvolution, self).__init__()
+        super().__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.weight = Parameter(torch.FloatTensor(in_features, out_features))#权重矩阵
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
         if bias:
-            self.bias = Parameter(torch.FloatTensor(out_features))#偏移向量
+            self.bias = Parameter(torch.FloatTensor(out_features))
         else:
             self.register_parameter('bias', None)
         self.reset_parameters()
@@ -38,50 +37,36 @@ class GraphConvolution(Module):
 
     def forward(self, input, adj):
         support = torch.mm(input, self.weight)
-        adj = adj.clone().detach()
-        support = support.clone().detach()
-        # adj = torch.tensor(adj, dtype=torch.float64)# torch.mm(a, b)是矩阵a和b矩阵相乘，torch.mul(a, b)是矩阵a和b对应位相乘，a和b的维度必须相等此处主要定义的是本层的前向传播，通常采用的是 A*X*W的计算方法。由于A是一个sparse变量，因此其与X进行卷积的结果也是稀疏矩阵。
-        # support = torch.tensor(support, dtype=torch.float64)
-        output = torch.spmm(adj.double(), support.double())# torch.spmm(a,b)是稀疏矩阵相乘
+        output = torch.spmm(adj.double(), support.double())
         if self.bias is not None:
             return output + self.bias
         else:
             return output
 
-    def __repr__(self):
-        return self.__class__.__name__ + ' (' \
-               + str(self.in_features) + ' -> ' \
-               + str(self.out_features) + ')'
 
 class GCN(nn.Module):
-    def __init__(self, nfeat, out):
-        super(GCN, self).__init__()
-        self.gc1 = GraphConvolution(nfeat, out)
+    def __init__(self, in_features: int, out_features: int):
+        super().__init__()
+        self.gc1 = LinearWithLoRA(
+            nn.Linear(in_features, out_features),
+            rank=4,
+            alpha=8
+        )
 
-    def forward(self, x, adj):
-        x = F.relu(self.gc1(x, adj))
-        return x
+    def forward(self, x):  # 移除了adj参数
+        return F.relu(self.gc1(x))
+
 
 class att_encoder(nn.Module):
-    """encoder in DA_RNN."""
-
-    def __init__(self,
-                 input_size,
-                 encoder_num_hidden,
-                 batch_size,
-                 window_length,
-                 parallel=False):
-        """Initialize an encoder in DA_RNN."""
-        super(att_encoder, self).__init__()
+    def __init__(self, input_size, encoder_num_hidden, batch_size, window_length, parallel=False):
+        super().__init__()
         self.encoder_num_hidden = encoder_num_hidden
         self.input_size = input_size
         self.batch_size = batch_size
         self.window_length = window_length
-        self.parallel = parallel
 
-        self.h_n = torch.nn.Parameter(torch.FloatTensor(1, batch_size, encoder_num_hidden), requires_grad=False)
-        self.s_n = torch.nn.Parameter(torch.FloatTensor(1, batch_size, encoder_num_hidden), requires_grad=False)
-
+        self.h_n = nn.Parameter(torch.FloatTensor(1, batch_size, encoder_num_hidden), requires_grad=False)
+        self.s_n = nn.Parameter(torch.FloatTensor(1, batch_size, encoder_num_hidden), requires_grad=False)
         torch.nn.init.uniform_(self.h_n, a=0, b=0)
         torch.nn.init.uniform_(self.s_n, a=0, b=0)
 
@@ -92,31 +77,16 @@ class att_encoder(nn.Module):
         )
 
         self.encoder_attn = nn.Linear(
-            in_features=2 * self.encoder_num_hidden + self.window_length,
-            out_features=1
+            2 * self.encoder_num_hidden + self.window_length,
+            1
         )
 
     def forward(self, X):
-        """forward.
-
-        Args:
-            X: input data
-
-        """
-
-        X_tilde = Variable(X.data.new(
-            X.size(0), self.window_length, self.input_size).zero_())
-
-        # batch_size * input_size * (2 * hidden_size + T - 1)
+        X_tilde = Variable(X.data.new(X.size(0), self.window_length, self.input_size).zero_())
         x = torch.cat((self.h_n.repeat(self.input_size, 1, 1).permute(1, 0, 2),
-                        self.s_n.repeat(self.input_size, 1, 1).permute(1, 0, 2),
-                        X.permute(0, 2, 1)), dim=2)
-
-        x = self.encoder_attn(
-            x.view(-1, self.encoder_num_hidden * 2 + self.window_length))
-
-
-        # get weights by softmax
+                       self.s_n.repeat(self.input_size, 1, 1).permute(1, 0, 2),
+                       X.permute(0, 2, 1)), dim=2)
+        x = self.encoder_attn(x.view(-1, self.encoder_num_hidden * 2 + self.window_length))
         alpha = F.softmax(x.view(-1, self.input_size), dim=1)
 
         for t in range(self.window_length):
@@ -124,51 +94,16 @@ class att_encoder(nn.Module):
             X_tilde[:, t, :] = x_tilde
 
         self.encoder_lstm.flatten_parameters()
-
         _, final_state = self.encoder_lstm(x_tilde.unsqueeze(0), (self.h_n, self.s_n))
         self.h_n = Parameter(final_state[0])
         self.s_n = Parameter(final_state[1])
-        X_tilde = X_tilde.view(-1, self.window_length * self.input_size)
-
-        return X_tilde
-
-def tabular_encoder(input_size: int, latent_size: int):
-    """
-    Simple encoder for tabular data.
-    If you want to feed image to a VAE make another encoder function with Conv2d instead of Linear layers.
-    :param input_size: number of input variables
-    :param latent_size: number of output variables i.e. the size of the latent space since it's the encoder of a VAE
-    :return: The untrained encoder model
-    """
-    return nn.Sequential(
-        nn.Linear(input_size, latent_size * 2),
-        # times 2 because this is the concatenated vector of latent mean and variance
-    )
-
-
-def tabular_decoder(latent_size: int, output_size: int):
-    """
-    Simple decoder for tabular data.
-    :param latent_size: size of input latent space
-    :param output_size: number of output parameters. Must have the same value of input_size
-    :return: the untrained decoder
-    """
-    return nn.Sequential(
-        nn.Linear(latent_size, output_size * 2),
-
-        # times 2 because this is the concatenated vector of reconstructed mean and variance
-    )
+        return X_tilde.view(-1, self.window_length * self.input_size)
 
 
 class MUTANT(nn.Module):
-
     def __init__(self, input_size: int, w_size, hidden_size: int, latent_size, batch_size, window_length, out_dim):
-        """
-        :param input_size: Number of input features
-        :param latent_size: Size of the latent space
-        :param L: Number of samples in the latent space (See paper for more details)
-        """
         super().__init__()
+        self.fc1 = LinearWithLoRA(nn.Linear(input_size, hidden_size))
         self.input_size = input_size
         self.w_size = w_size
         self.latent_size = latent_size
@@ -177,103 +112,110 @@ class MUTANT(nn.Module):
         self.att_encoder = att_encoder(input_size, hidden_size, batch_size, out_dim)
         self.encoder = tabular_encoder(w_size, latent_size)
         self.decoder = tabular_decoder(latent_size, w_size)
-        self.GCN = GCN(window_length, out_dim)
+        self.GCN = GCN(in_features=window_length, out_features=out_dim)
         self.prior = Normal(0, 1)
 
     def forward(self, x):
         Xt = []
         l0 = 0
         for i in x:
-            A = torch.tensor(returnA(i))
-            x_g = self.GCN(i.permute(1, 0), A).permute(1, 0)
+            # 移除了A相关的所有代码
+            x_g = self.GCN(i.permute(1, 0)).permute(1, 0)
             loss = GCN_Loss(x_g)
             l0 = loss
-            x_g = x_g.detach().numpy()
-            Xt.append(x_g)
+            Xt.append(x_g.detach().numpy())
+
         Xt = torch.tensor(Xt, dtype=torch.float32)
         X_w = self.att_encoder(Xt)
         pred_result = self.predict(X_w)
         mu = pred_result['recon_mu']
+
         Xt = Xt.view(-1, Xt.shape[1] * Xt.shape[2])
-        loss_function = torch.nn.MSELoss()
-        l1 = loss_function(mu, Xt)
-        l2 = torch.mean(-0.5 * torch.sum(1 + pred_result['latent_sigma'] - pred_result['latent_mu'].pow(2) - pred_result['latent_sigma'].exp(), dim=1), dim=0)
-        loss = l0 + l1 + l2
-        return loss
+        l1 = F.mse_loss(mu, Xt)
+        l2 = torch.mean(-0.5 * torch.sum(
+            1 + pred_result['latent_sigma'] -
+            pred_result['latent_mu'].pow(2) -
+            pred_result['latent_sigma'].exp(), dim=1))
+
+        return l0 + l1 + l2
 
     def predict(self, x) -> dict:
-        """
-        :param x: tensor of shape [batch_size, num_features]
-        :return: A dictionary containing prediction i.e.
-        - latent_dist = torch.distributions.Normal instance of latent space
-        - latent_mu = torch.Tensor mu (mean) parameter of latent Normal distribution
-        - latent_sigma = torch.Tensor sigma (std) parameter of latent Normal distribution
-        - recon_mu = torch.Tensor mu (mean) parameter of reconstructed Normal distribution
-        - recon_sigma = torch.Tensor sigma (std) parameter of reconstructed Normal distribution
-        - z = torch.Tensor sampled latent space from latent distribution
-        """
         batch_size = len(x)
         latent_mu, latent_sigma = self.encoder(x).chunk(2, dim=1)
         latent_sigma = softplus(latent_sigma) + 1e-4
         dist = Normal(latent_mu, latent_sigma)
-        z = dist.rsample()
-        z = z.view(batch_size, self.latent_size)
+        z = dist.rsample().view(batch_size, self.latent_size)
+
         recon_mu, recon_sigma = self.decoder(z).chunk(2, dim=1)
         recon_sigma = softplus(recon_sigma) + 1e-4
-        recon_mu = recon_mu.view(-1, x.shape[1])
-        recon_sigma = recon_sigma.view(-1, x.shape[1])
-
-        return dict(latent_dist=dist, latent_mu=latent_mu,
-                latent_sigma=latent_sigma, recon_mu=recon_mu,
-                recon_sigma=recon_sigma, z=z)
-
+        return {
+            'latent_dist': dist,
+            'latent_mu': latent_mu,
+            'latent_sigma': latent_sigma,
+            'recon_mu': recon_mu.view(-1, x.shape[1]),
+            'recon_sigma': recon_sigma.view(-1, x.shape[1]),
+            'z': z
+        }
 
     def is_anomaly(self, x, num_t, con_t):
-        """
-
-        :param x:
-        :param alpha: Anomaly threshold (see paper for more details)
-        :return: Return a vector of boolean with shape [x.shape[0]]
-                 which is true when an element is considered an anomaly
-        """
         score = []
-        np.set_printoptions(threshold=999999999999999999)
-        for i, inputs in enumerate(x, 0):
+        for i, inputs in enumerate(x):
             p = self.reconstructed_probability(inputs)
-            if i==num_t:
+            if i == num_t:
                 p = p[:con_t]
-            score = np.concatenate((score, p), axis=0)
-        score = np.array(score)
-        return score
+            score = np.concatenate((score, p))
+        return np.array(score)
 
     def reconstructed_probability(self, x):
         with torch.no_grad():
             Xt = []
             for i in x:
-                A = torch.tensor(returnA(i))
-                x_g = self.GCN(i.permute(1, 0), A).permute(1, 0).detach().numpy()
+                # 移除了A相关的所有代码
+                x_g = self.GCN(i.permute(1, 0)).permute(1, 0).detach().numpy()
                 Xt.append(x_g)
+
             Xt = torch.tensor(Xt, dtype=torch.float32)
             X_w = self.att_encoder(Xt)
             pred = self.predict(X_w)
             mu = pred['recon_mu']
+
             Xt = Xt.view(-1, Xt.shape[1] * Xt.shape[2])
-            p = []
-            for i in range(x.shape[0]):
-                t = abs(torch.sum((Xt[i] - mu[i])))
-                p.append(t)
-        p = np.array(p)
-        return p
-
-    def generate(self, batch_size: int=1) -> torch.Tensor:
-        """
-        Sample from prior distribution, feed into decoder and get in output recostructed samples
-        :param batch_size:
-        :return: Generated samples
-        """
-        z = self.prior.sample((batch_size, self.latent_size))
-        recon_mu, recon_sigma = self.decoder(z).chunk(2, dim=1)
-        recon_sigma = softplus(recon_sigma)
-        return recon_mu + recon_sigma * torch.rand_like(recon_sigma)
+            return np.array([abs(torch.sum(Xt[i] - mu[i])).item() for i in range(x.shape[0])])
 
 
+class LoRALayer(nn.Module):
+    def __init__(self, in_dim, out_dim, rank, alpha):
+        super().__init__()
+        self.A = nn.Parameter(torch.randn(in_dim, rank))
+        self.B = nn.Parameter(torch.zeros(rank, out_dim))
+        self.alpha = alpha
+
+    def forward(self, x):
+        return self.alpha * (x @ self.A @ self.B)
+
+
+class LinearWithLoRA(nn.Module):
+    def __init__(self, linear_layer, rank=8, alpha=16):
+        super().__init__()
+        self.linear = linear_layer
+        self.lora = LoRALayer(linear_layer.in_features,
+                              linear_layer.out_features,
+                              rank, alpha)
+        # 冻结原始参数
+        for param in self.linear.parameters():
+            param.requires_grad = False
+
+    def forward(self, x):
+        return self.linear(x) + self.lora(x)
+
+
+def tabular_encoder(input_size: int, latent_size: int):
+    return nn.Sequential(
+        nn.Linear(input_size, latent_size * 2),
+    )
+
+
+def tabular_decoder(latent_size: int, output_size: int):
+    return nn.Sequential(
+        nn.Linear(latent_size, output_size * 2),
+    )
--- main.py
+++ main.py
@@ -1,134 +1,150 @@
+# main.py
 import torch
 import numpy as np
 import pandas as pd
+import os
 import torch.optim as optim
+from tqdm import tqdm
+from Model import MUTANT
 from utils import get_data, get_data_dim, get_loader
 from eval_method import bf_search
-from tqdm import tqdm
 
-from Model import MUTANT
 
-class ExpConfig():
+class ExpConfig:
     dataset = "SWaT"
-    val = 0.35  # the ratio of validation set
-    max_train_size = None  # `None` means full train set
+    val = 0.35
+    max_train_size = None
     train_start = 0
-
-    max_test_size = None  # `None` means full test set
+    max_test_size = None
     test_start = 0
-
     input_dim = get_data_dim(dataset)
     batch_size = 120
-
-    out_dim = 5   # the dimension of embedding
+    out_dim = 5
     window_length = 20
-    hidden_size = 100  # the dimension of hidden layer in LSTM-based attention
-    latent_size = 100  # the dimension of hidden layer in VAE
+    hidden_size = 100
+    latent_size = 100
     N = 256
+    lora_rank = 4
+    lora_alpha = 8
+    lora_layers = ['fc1', 'gc1']
+
 
 def main():
     config = ExpConfig()
-
-    (train_data, _), (test_data, test_label) = \
-        get_data(config.dataset, config.max_train_size, config.max_test_size, train_start=config.train_start,
-                 test_start=config.test_start)
-
-    n = int(test_data.shape[0] * config.val)
-    test_data = test_data[:-n]
-    test_label = test_label[:-n]
-
-    val_data = test_data[-n:]
-    val_label = test_label[-n:]
-
-    print("test_data:", test_data.shape)
-    print("val_data:", val_data.shape)
-
-    train_data = train_data[np.arange(config.window_length)[None, :] + np.arange(train_data.shape[0] - config.window_length)[:, None]]
-    val_data = val_data[np.arange(config.window_length)[None, :] + np.arange(val_data.shape[0] - config.window_length)[:, None]]
-    test_data = test_data[np.arange(config.window_length)[None, :] + np.arange(test_data.shape[0] - config.window_length)[:, None]]
-
-    num_val = int(val_data.shape[0]/config.batch_size)
-    con_val = val_data.shape[0] % config.batch_size
-    num_t = int(test_data.shape[0]/config.batch_size)
-    con_t = test_data.shape[0] % config.batch_size
-
-    w_size = config.input_dim * config.out_dim
-
-    train_loader = get_loader(train_data, batch_size=config.batch_size,
-                              window_length=config.window_length, input_size=config.input_dim, shuffle=True)
-    val_loader = get_loader(val_data, batch_size=config.batch_size,
-                              window_length=config.window_length, input_size=config.input_dim, shuffle=True)
-    test_loader = get_loader(test_data, batch_size=config.batch_size,
-                              window_length=config.window_length, input_size=config.input_dim, shuffle=False)
-
-    model = MUTANT(config.input_dim, w_size, config.hidden_size, config.latent_size, config.batch_size, config.window_length, config.out_dim)
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
-
-    print("Starting training...")
     save_path = 'model.pt'
-    flag = 0
-    f1 = -1
-    for epoch in range(10):
-        l = 0
-        i = 0
-        for inputs in tqdm(train_loader):
+
+    # 数据加载
+    (train_data, _), (test_data, test_label) = get_data(config.dataset,
+                                                        config.max_train_size,
+                                                        config.max_test_size,
+                                                        train_start=config.train_start,
+                                                        test_start=config.test_start)
+
+    # 数据分割
+    n = int(test_data.shape[0] * config.val)
+    test_data, val_data = test_data[:-n], test_data[-n:]
+    test_label, val_label = test_label[:-n], test_label[-n:]
+
+    # 窗口处理
+    def window_data(data):
+        return data[np.arange(config.window_length)[None, :] +
+                    np.arange(data.shape[0] - config.window_length)[:, None]]
+
+    train_data = window_data(train_data)
+    val_data = window_data(val_data)
+    test_data = window_data(test_data)
+
+    # 数据加载器
+    w_size = config.input_dim * config.out_dim
+    train_loader = get_loader(train_data, config.batch_size,
+                              config.window_length, config.input_dim, True)
+    val_loader = get_loader(val_data, config.batch_size,
+                            config.window_length, config.input_dim, True)
+    test_loader = get_loader(test_data, config.batch_size,
+                             config.window_length, config.input_dim, False)
+
+    # 模型初始化
+    model = MUTANT(config.input_dim, w_size, config.hidden_size,
+                   config.latent_size, config.batch_size,
+                   config.window_length, config.out_dim)
+
+    # 加载检查点
+    start_epoch, best_f1 = 0, -1
+    if os.path.exists(save_path):
+        checkpoint = torch.load(save_path)
+        model.load_state_dict(checkpoint['model_state'])
+        print(f"Loaded checkpoint: epoch {checkpoint['epoch']}, best f1 {checkpoint['best_f1']:.4f}")
+        start_epoch = checkpoint['epoch'] + 1
+        best_f1 = checkpoint['best_f1']
+
+    # 优化器设置
+    trainable_params = [p for n, p in model.named_parameters()
+                        if any(l in n for l in config.lora_layers)]
+    optimizer = optim.Adam(trainable_params, lr=0.01)
+
+    # 训练循环
+    for epoch in range(start_epoch, 10):
+        model.train()
+        pbar = tqdm(train_loader)
+        for i, inputs in enumerate(pbar):
+            optimizer.zero_grad()
             loss = model(inputs)
             loss.backward()
-            if(i% config.N == 0):
+
+            # 梯度裁剪
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+            if i % config.N == 0:
                 optimizer.step()
                 optimizer.zero_grad()
-            i += 1
 
-            
-        if(flag == 1):
-            model.load_state_dict(torch.load(save_path))
-        val_score = model.is_anomaly(val_loader, num_val, con_val)
-        t, th = bf_search(val_score, val_label[-len(val_score):], step_num=700)
-        if(t[0] > f1):
-            f1 = t[0]
-            torch.torch.save(model.state_dict(), save_path)
-            flag = 1
+            pbar.set_postfix(loss=loss.item())
 
-    model.load_state_dict(torch.load(save_path))
-    test_score = model.is_anomaly(test_loader, num_t, con_t)
+        # 验证
+        model.eval()
+        val_score = model.is_anomaly(val_loader,
+                                     len(val_loader),
+                                     len(val_loader.dataset) % config.batch_size)
+        t, th = bf_search(val_score, val_label[-len(val_score):], 700)
 
-    t, th = bf_search(test_score, test_label[-len(test_score):], step_num=700)
-    print('*****************************************************')
-    print('dataset:', config.dataset)
-    print('th:', th)
-    print("TP:", t[3])
-    print("FP:", t[5])
-    print("FN:", t[6])
-    print("precision:", t[1], "recall:", t[2], "f_score", t[0])
+        # 保存最佳模型
+        if t[0] > best_f1:
+            best_f1 = t[0]
+            torch.save({
+                'epoch': epoch,
+                'model_state': model.state_dict(),
+                'best_f1': best_f1,
+                'lora_config': {
+                    'rank': config.lora_rank,
+                    'alpha': config.lora_alpha,
+                    'layers': config.lora_layers
+                }
+            }, save_path)
+            print(f"New best model saved at epoch {epoch} with f1 {best_f1:.4f}")
 
-    # 将结果保存为CSV文件
-    results = {
+    # 最终测试
+    model.load_state_dict(torch.load(save_path)['model_state'])
+    test_score = model.is_anomaly(test_loader,
+                                  len(test_loader),
+                                  len(test_loader.dataset) % config.batch_size)
+    t, th = bf_search(test_score, test_label[-len(test_score):], 700)
+
+    # 保存结果
+    results = pd.DataFrame({
         'Dataset': [config.dataset],
         'Threshold': [th],
-        'TP': [t[3]],
-        'FP': [t[5]],
-        'FN': [t[6]],
-        'Precision': [t[1]],
-        'Recall': [t[2]],
-        'F-Score': [t[0]]
-    }
+        'TP': [t[3]], 'FP': [t[5]], 'FN': [t[6]],
+        'Precision': [t[1]], 'Recall': [t[2]], 'F-Score': [t[0]]
+    })
 
-    # 将字典转换为DataFrame
-    df_result = pd.DataFrame(results)
-
-    # 检查CSV文件是否存在
     try:
-        # 如果文件存在，读取文件并将新数据追加到最后一行
-        existing_df = pd.read_csv('results.csv')
-        combined_df = pd.concat([existing_df, df_result], ignore_index=True)
+        existing = pd.read_csv('results.csv')
+        results = pd.concat([existing, results])
     except FileNotFoundError:
-        # 如果文件不存在，直接使用当前数据
-        combined_df = df_result
+        pass
 
-    # 将结果保存到CSV文件
-    combined_df.to_csv('results.csv', index=False)
+    results.to_csv('results.csv', index=False)
 
-    return t[1], t[2], t[0]
 
 if __name__ == '__main__':
-   main()
+    main()
